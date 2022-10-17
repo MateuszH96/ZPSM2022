64e20bce40df49dd344876c3b6ec5be1c9114c68
MateuszHamera <mateuszhamera2@gmail.com>
2022-10-17 22:46:05 +0200

    zadanie3

--- lab2/app.js
+++ lab2/app.js
@@ -8,9 +8,49 @@ function find_min(arr){
     return _.min(arr)
 }
 
+function viewInfo(toInfo){
+    console.log(toInfo.name)
+    console.log(toInfo.surname)
+    toAverageU = 0
+    toAverageD = 0
+    for(const grades of toInfo.allGrades){
+        toAverageU += _.mean(grades.grades) * grades.weight
+        toAverageD += grades.weight
+    }
+    console.log(toAverageU/toAverageD)
+
+}
+
 console.log(find_max([1,2,3,4,5]))
 console.log(find_min([1,2,3,4,5]))
 
 
 a=_.mean([1,2,3,4])
 console.log(a)
+
+
+
+const user = {
+    name: 'Imie',
+    surname: 'Nazwisko',
+    allGrades: [
+        {
+            subjectName: 'Name1',
+            grades: [5,4,3,5,2],
+            weight: 3
+        },
+        {
+            subjectName: 'Name2',
+            grades: [3,3.5,2],
+            weight: 1
+        },
+        {
+            subjectName: 'Name3',
+            grades: [4,3,3.5],
+            weight: 5
+        }
+    ]
+
+}
+
+viewInfo(user)
